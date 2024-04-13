da2f4edb026af3636471d6993d33088b977addf1
ThengggTapCode <samthang6b@gmail.com>
2024-04-13 19:53:45 +0700

    styling and improved login

--- actions/js/login-account.js
+++ actions/js/login-account.js
@@ -1,15 +1,37 @@
+// incorrect username
+function incorrectUsername() {
+  let newP = document.createElement("p"); // create a new p tag
+  let usernameStatus = document.getElementById("username-status");
+
+  newP.textContent = "Incorrect Username"; // content for p tag
+  usernameStatus.innerHTML = ""; // remove former status
+  usernameStatus.appendChild(newP); // assign content for status
+}
+// incorrect password
+function incorrectPassword() {
+  let newP = document.createElement("p"); // create a new p tag
+  let passwordStatus = document.getElementById("password-status");
+
+  newP.textContent = "Incorrect Password"; // content for p tag
+  passwordStatus.innerHTML = ""; // remove former status
+  passwordStatus.appendChild(newP); // assign content for status
+}
+// check input data
 function checkData(inpUsername, inpPassword) {
   const validUsername = "ThengggTapCode";
   const validPassword = "jqka2";
 
   if (inpUsername !== validUsername) {
+    incorrectUsername();
     return false;
   }
   if (inpPassword !== validPassword) {
+    incorrectPassword();
     return false;
   }
   return true;
 }
+// login successful
 function loginSuccessful(inpUsername) {
   let newH1 = document.createElement("h1"); // make a new h1 tag
   let loginStatus = document.getElementById("login-status");
@@ -18,6 +40,7 @@ function loginSuccessful(inpUsername) {
   loginStatus.innerHTML = ""; // remove former login status
   loginStatus.appendChild(newH1); // assign h1 content for login status
 }
+// login failed
 function loginFailed() {
   let newH1 = document.createElement("h1"); // make a new h1 tag
   let loginStatus = document.getElementById("login-status");
@@ -26,6 +49,7 @@ function loginFailed() {
   loginStatus.innerHTML = ""; // remove former login status
   loginStatus.appendChild(newH1); // assign h1 content for login status
 }
+// main
 function main() {
   // get data from html
   let inpUsername = document.getElementById("inp-username").value;
